6d7256814b1f5d41e2c02840fb691c785aa7ff9d
Dru Mumba <drusademumba@gmail.com>
2024-02-12 16:58:32 +0300

    Style changes

--- web_flask/static/scripts/index.js
+++ web_flask/static/scripts/index.js
@@ -1,5 +1,5 @@
 function redirectToUserPage() {
   setTimeout(function() {
-      window.location.href = "http://127.0.0.1:5000/search/drusademumba";
-  }, 5000);
+      window.location.href = "http://127.0.0.1:5000/search/github";
+  }, 3000);
 }
