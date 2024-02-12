c74537198f035c66b0ac6fc68643b2c75856228e
Dru Mumba <drusademumba@gmail.com>
2024-02-12 21:07:30 +0300

    Add firebase user profile

--- web_flask/views/profile.py
+++ web_flask/views/profile.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
 """Firebase user profile"""
-import pyrebase
-from flask import redirect, url_for, session, render_template
+from firebase_admin import db
+from flask import redirect, url_for, render_template
 from web_flask import app_views
-from web_flask.config import Config
-
-app_config = Config()
-firebaseConfig = app_config.FIREBASE_CONFIG
-
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
 
 
-@app_views.route("/profile", methods=["POST", "GET"], strict_slashes=False)
+@app_views.route("/profile", strict_slashes=False)
 def my_profile():
-    if 'is_logged_in' in session and session['is_logged_in']:
-        return render_template('profile.html', uid=session["uid"])
-    else:
-        return redirect(url_for('app_views.login'))
+    return redirect(url_for('app_views.profile'))
 
 
-@app_views.route("/profile/<uid>", methods=["POST", "GET"], strict_slashes=False)
+@app_views.route("/profile/<uid>", strict_slashes=False)
 def profile(uid):
-    if 'is_logged_in' in session and session['is_logged_in']:
-        return render_template('profile.html', uid=uid, email=session["email"],
-                               username=session["username"])
+    uid_ref = db.reference('users/' + uid)
+    uid_data = uid_ref.get()
+    if uid_data:
+        username = uid_data.get('username')
+        email = uid_data.get('email')
+        return render_template('profile.html', uid=uid, username=username, email=email)
     else:
-        return redirect(url_for('app_views.login'))
+        return 'UID not found in the database', 404
 
 
 @app_views.route("/logout")
 def logout():
-    session.clear()
-    return redirect(url_for('app_views.gituser'))
+    return redirect(url_for('app_views.search'))
