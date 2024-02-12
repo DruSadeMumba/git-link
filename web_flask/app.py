3433869417a72dcf26603195a81a23f7c87ad5dc
Dru Mumba <drusademumba@gmail.com>
2024-02-12 21:06:11 +0300

    Initialise firebase

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Flask Application """
+import firebase_admin
+from firebase_admin import credentials
 from flask import Flask
 from flask_cors import CORS
 from web_flask.config import Config
@@ -9,6 +11,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = app.config['SECRET_KEY']
 app.register_blueprint(app_views)
+cred = credentials.Certificate("web_flask/views/git-link-d9cc9-firebase-adminsdk-o2gwi-821d140e51.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://git-link-d9cc9-default-rtdb.firebaseio.com/'})
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
