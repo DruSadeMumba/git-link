5d36dd57efd786bf95858417af8cf3f0eae55aca
Dru Mumba <drusademumba@gmail.com>
2024-02-12 21:06:55 +0300

    Remove redundant route

--- web_flask/views/signup.py
+++ web_flask/views/signup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 """Firebase user signup"""
-
-from flask import render_template, jsonify
+from flask import render_template
 from web_flask import app_views
-from web_flask.config import Config
 
 
 @app_views.route("/signup", strict_slashes=False)
 def signup():
     return render_template("sign-up.html")
-
-
-@app_views.route('/firebase-config')
-def get_firebase_config():
-    return jsonify(Config.FIREBASE_CONFIG)
