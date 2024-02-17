f89838c4cc53e5041afa8ec7adaf108970060904
Dru Mumba <drusademumba@gmail.com>
2024-02-17 08:23:06 +0300

    Add share options

--- web_flask/static/scripts/user.js
+++ web_flask/static/scripts/user.js
@@ -17,24 +17,36 @@ document.getElementById("search-input").addEventListener("keypress", (e) => {
   }
 });
 
-save_repo = (uid, name, description, html_url) => {
-  $.ajax({
-    type: 'POST',
-    url: '/save_repo/${uid}',
-    data: {
-      uid: uid,
-      name: name,
-      description: description,
-      html_url: html_url
-    },
-    success: (response) => {
-      console.log(response)
-      alert('Repository saved successfully!');
-      },
-    error: (xhr, status, error) => {
-      console.error(error);
-      alert('Failed to save repository.');
-    }
-  });
-}
+const shareOptions = () => {
+  closeNav();
+  document.getElementById("social-share").style.width = "400px";
+  document.querySelector(".wrapper").style.opacity = "0.3";
+};
+
+const closeShare = () => {
+  document.getElementById("social-share").style.width = "0px";
+  document.querySelector(".wrapper").style.opacity = "1";
+  openNav();
+};
+
+const links = encodeURI("https://github.com/DruSadeMumba/git-link");
+const msg = encodeURIComponent('Hey, check this repo out');
+const title = encodeURIComponent('GIT-LINK');
+const fb = document.querySelector('.facebook');
+const twitter = document.querySelector('.twitter');
+const linkedIn = document.querySelector('.linkedin');
+const reddit = document.querySelector('.reddit');
+const whatsapp = document.querySelector('.whatsapp');
+const telegram = document.querySelector('.telegram');
+
+
+
+fb.href = `https://www.facebook.com/share.php?u=${links}`;
+twitter.href = `https://twitter.com/share?&url=${links}&text=${msg}&hashtags=github,alx`;
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${links}`;
+reddit.href = `https://www.reddit.com/submit?url=${links}&title=${title}`;
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${links}`;
+telegram.href = `https://telegram.me/share/url?url=${links}&text=${msg}`;
+
+
 
