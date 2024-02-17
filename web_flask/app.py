792d4bb75930114f113b5ab32c1e0e04c7f9c2d8
DruSadeMumba <drusademumba@gmail.com>
2024-02-17 11:30:32 +0000

    Change port

--- web_flask/app.py
+++ web_flask/app.py
@@ -17,4 +17,4 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5004, threaded=True)
