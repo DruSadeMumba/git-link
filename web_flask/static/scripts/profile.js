80c31ae2147c043e2fa775258937e7282159ede1
Dru Mumba <drusademumba@gmail.com>
2024-02-20 16:00:57 +0300

    Dry code

--- web_flask/static/scripts/profile.js
+++ web_flask/static/scripts/profile.js
@@ -56,30 +56,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
-const shareRepo = (url) => {
-  const links = encodeURI(url);
-  const txt = encodeURIComponent('Hey, check out this repo');
-  const title = encodeURIComponent('');
-  const fb = document.querySelector('.facebook');
-  const twitter = document.querySelector('.twitter');
-  const linkedIn = document.querySelector('.linkedin');
-  const reddit = document.querySelector('.reddit');
-  const whatsapp = document.querySelector('.whatsapp');
-  const telegram = document.querySelector('.telegram');
-
-  fb.href = `https://www.facebook.com/share.php?u=${links}`;
-  twitter.href = `https://twitter.com/share?&url=${links}&text=${txt}&hashtags=github,alx`;
-  linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${links}`;
-  reddit.href = `https://www.reddit.com/submit?url=${links}&title=${title}`;
-  whatsapp.href = `https://api.whatsapp.com/send?text=${txt}: ${links}`;
-  telegram.href = `https://telegram.me/share/url?url=${links}&text=${txt}`;
-  
-  document.getElementById("social-share").style.width = "400px";
-  document.querySelector(".wrapper").style.opacity = "0.4";
-}
-
-const closeShare = () => {
-  document.getElementById("social-share").style.width = "0px";
-  document.querySelector(".wrapper").style.opacity = "1";
-};
